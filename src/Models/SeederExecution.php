938219b927c9bafd27df1ec8f7cb7342aab9f078
Leandro Santos <geral@riftweb.com>
2025-08-09 13:45:09 +0100

    Fixing updated_at removal

--- src/Models/SeederExecution.php
+++ src/Models/SeederExecution.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class SeederExecution extends Model
 {
+    const UPDATED_AT = null;
+    public $timestamps = ['created_at'];
     protected $fillable = [
         'seeder',
         'batch'
